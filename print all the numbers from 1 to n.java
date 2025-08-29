c4c8ebc18ecff37d849399e34451352004c53e39
Harish <147738686+harish07102004@users.noreply.github.com>
2025-08-29 20:44:41 +0530

    print all the numbers from 1 to n.java

---
+++ print all the numbers from 1 to n.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n,i=1;
+        n= sc.nextInt();
+        do{
+            System.out.print(i+" ");
+            i++;
+        }while(i<=n);
+    }
+}
